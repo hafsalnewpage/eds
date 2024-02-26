2f513812b0db56a38e6ad2f3c7bd190c02dd6bf6
Hafsal <hafsal@Hafsals-MacBook-Pro.local>
2024-02-26 13:30:43 +0530

    dom modification for blocks

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,15 +1,14 @@
 export default function decorate(block) {
-  
-  [...block.children].forEach((row, r) => {
-      [...row.children].forEach((column, c) => {  
-          if(c === 2) {
-            const ul = document.createElement('ul');
-            const li = document.createElement('li');
-            const text = document.createTextNode('Health');
-            li.append(text);
-            ul.append(li);
-            column.replaceWith(ul);
-        }
+    [...block.children].forEach((row, r) => {
+        [...row.children].forEach((column, c) => {
+            if (c === 2) {
+                const ul = document.createElement('ul');
+                const li = document.createElement('li');
+                const text = document.createTextNode('Health');
+                li.append(text);
+                ul.append(li);
+                column.replaceWith(ul);
+            }
+        });
     });
-  });
 }
